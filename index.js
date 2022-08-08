f36061ce894e40d6f5730499eb8458c8048caa1f
cyber-ahs <alihassam@Alis-MacBook-Air.local>
2022-08-08 03:49:14 -0700

    first

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('This is my First Code')
 })
 const PORT =process.env.PORT || 3000;
 app.listen(PORT, () => {
