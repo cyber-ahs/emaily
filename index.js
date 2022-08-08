ea98450a3acc55ada0ecda798b6736de44d44173
cyber-ahs <alihassam@Alis-MacBook-Air.local>
2022-08-08 07:05:20 -0700

    first

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 const express   = require('express');
+const passport = require('passport');
+const { googleClientSecret } = require('./config/key');
 const app = express()
 const port = 3000
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+const key =require('./config/key');
 
-app.get('/', (req, res) => {
-  res.send('This is my First Code')
-})
-const PORT =process.env.PORT || 3000;
+passport.use(
+  new GoogleStrategy({
+  clientID: key.googleClientID,
+  ClientSecret: key.googleClientSecret, 
+  callbackURL: '/auth/google/callback',
+  }, (accesstoken) =>{
+    console.log(accesstoken);
+  })
+
+);
+app.get(
+  '/auth/google',
+passport.authenticate('google',{
+  scope: ['profile','email']
+ })
+);
+
+  //ClientID 1099486538368-k1ikogouos2e612vh6g5v5akh1dhtuph.apps.googleusercontent.com
+//ClientSecret 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
+
+const PORT =process.env.PORT || 5001;
 app.listen(PORT, () => {
 
 })
