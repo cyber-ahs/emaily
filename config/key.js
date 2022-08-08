4204d7aced1f4775a234a0f5c0342ca8fd20df8c
cyber-ahs <alihassam@Alis-MacBook-Air.local>
2022-08-08 08:46:44 -0700

    first commit

---
+++ config/key.js
@@ -0,0 +1,5 @@
+module.exports = {
+    googleClientID: '1099486538368-k1ikogouos2e612vh6g5v5akh1dhtuph.apps.googleusercontent.com',
+    googleClientSecret: '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'
+  
+};
